cb13c087251727985f369472c5ed0d0cd3064117
Hassan12ee <hassan123.hhnn@gmail.com>
2024-10-16 19:06:46 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./layouts/includes/footer/footer.component";
-import { NavbarComponent } from './layouts/includes/navbar/navbar.component';
+import { FooterComponent } from "./layout/includes/footer/footer.component";
+import { NavbarComponent } from "./layout/includes/navbar/navbar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent ,NavbarComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
